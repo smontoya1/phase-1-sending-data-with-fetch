5b33ff077d67d7c1c8a26ad1fb5dfd7dbd8d67e4
Shaun <smontoya0009@gmail.com>
2025-05-17 15:36:24 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData (name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": "Bearer test-token"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        const body = document.querySelector('body')
+        body.innerHTML = data.id
+        console.log(data)
+    })
+    .catch(error => {
+        const body = document.querySelector('body')
+        body.innerHTML = error
+    })
+    
+}
+
